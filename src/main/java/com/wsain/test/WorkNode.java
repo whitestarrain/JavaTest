61b4df7a732d01fb5f0eb7c1169ada0e3f2f953c
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2022-11-06 22:33:01 +0800

    WorkNodeScheduling问题完成，未测试

---
+++ src/main/java/com/wsain/test/WorkNode.java
@@ -0,0 +1,104 @@
+package com.wsain.test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * InnerWorkNode
+ */
+@FunctionalInterface
+interface WorkNodeCallback {
+    public void run(WorkNode currentNode);
+}
+
+public class WorkNode extends Thread {
+
+    private static Random rand = new Random();
+
+    private static volatile int count = 0;
+    private int nodeCount;
+    private int runtime;
+    private String workId;
+    public List<WorkNode> nextNodes;
+    private boolean finishedStatus = false;
+
+    WorkNodeCallback callback;
+
+    public WorkNode() {
+        this((workNode) -> {
+        });
+    }
+
+    public WorkNode(WorkNodeCallback callback) {
+        this.nodeCount = ++count;
+        this.workId = "worknode" + this.nodeCount;
+        this.runtime = rand.nextInt(1000);
+        this.callback = callback;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.workId.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return this.workId;
+    }
+
+    public String getWorkId() {
+        return this.workId;
+    }
+
+    public boolean getFinishedStatus(){
+        return this.finishedStatus;
+    }
+
+    private String getRuningMessage() {
+        return this.workId +
+                new String(new char[this.nodeCount])
+                        .replace("\0", "-");
+    }
+
+    public static WorkNode getWorkTree(WorkNodeCallback callback) {
+        WorkNode w1 = new WorkNode(callback);
+        WorkNode w2 = new WorkNode(callback);
+        WorkNode w3 = new WorkNode(callback);
+        WorkNode w4 = new WorkNode(callback);
+        WorkNode w5 = new WorkNode(callback);
+        WorkNode w6 = new WorkNode(callback);
+        WorkNode w7 = new WorkNode(callback);
+
+        List<WorkNode> w1Childs = Arrays.asList(w2, w3);
+        w1.nextNodes = w1Childs;
+
+        List<WorkNode> w2Childs = Arrays.asList(w3, w4, w5);
+        w2.nextNodes = w2Childs;
+
+        List<WorkNode> w3Childs = Arrays.asList(w5, w6);
+        w3.nextNodes = w3Childs;
+
+        List<WorkNode> w6Childs = Arrays.asList(w7);
+        w6.nextNodes = w6Childs;
+
+        return w1;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.getRuningMessage() + "start" + "(" + this.runtime + ")");
+            // work
+            Thread.sleep(this.runtime);
+            // finish
+            System.out.println(this.getRuningMessage() + "end" + "(" + this.runtime + ")");
+            this.finishedStatus=true;
+            if (null != this.callback) {
+                this.callback.run(this);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/wsain/test/WorkNodeScheduling.java
@@ -0,0 +1,80 @@
+
+package com.wsain.test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class WorkNodeScheduling {
+    // 也可以不使用volatile，添加锁
+    // 这里统计任务的结束完成数量来判断是否进行完成所有任务。
+    // 也可以使用正在运行的任务，但是实际写起来有些别扭。
+    public volatile static int finishCount;
+
+    public static void main(String[] args) throws InterruptedException {
+        Map<String, Set<WorkNode>> parentMap = new HashMap<>();
+        BlockingQueue<WorkNode> runnableNodeQueue = new LinkedBlockingQueue<>();
+        WorkNodeCallback callback = (curWorkNode) -> {
+            finishCount++;
+            if (null == curWorkNode.nextNodes || 0 == curWorkNode.nextNodes.size()) {
+                return;
+            }
+
+            for (WorkNode nextNode : curWorkNode.nextNodes) {
+                Set<WorkNode> parentNodeSet = parentMap.get(nextNode.getWorkId());
+                for (WorkNode parentNode : parentNodeSet) {
+                    if (!parentNode.getFinishedStatus()) {
+                        return;
+                    }
+                    try {
+                        runnableNodeQueue.put(nextNode);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        };
+        WorkNode root = WorkNode.getWorkTree(callback);
+        getParnentMap(parentMap, root);
+        Set<String> allWorkNodeIdSet = new HashSet<>();
+        for (Entry<String, Set<WorkNode>> entrySet : parentMap.entrySet()) {
+            allWorkNodeIdSet.add(entrySet.getKey());
+            for (WorkNode value : entrySet.getValue()) {
+                allWorkNodeIdSet.add(value.getWorkId());
+            }
+        }
+        int allNodeNumber = allWorkNodeIdSet.size();
+        runnableNodeQueue.put(root);
+        while (finishCount < allNodeNumber) {
+            // 队列 阻塞取，每次只获取一个。
+            WorkNode runnableNode = runnableNodeQueue.take();
+            // 获取节点开始执行
+            if (1 == allNodeNumber - finishCount){
+                // 最后一个执行节点使用主线程执行。
+                runnableNode.run();
+            }else{
+                runnableNode.start();
+            }
+        }
+        System.out.println("执行完成");
+    }
+
+    public static void getParnentMap(Map<String, Set<WorkNode>> parentMap, WorkNode node) {
+        List<WorkNode> nextNodes = node.nextNodes;
+        if (null == nextNodes || nextNodes.size() == 0) {
+            return;
+        }
+        for (WorkNode nextNode : nextNodes) {
+            Set<WorkNode> parentNodeList = parentMap.computeIfAbsent(nextNode.getWorkId(),
+                    key -> new HashSet<WorkNode>());
+            parentNodeList.add(node);
+            getParnentMap(parentMap, nextNode);
+        }
+    }
+}
